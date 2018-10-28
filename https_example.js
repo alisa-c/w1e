4c3d868a50f04796b660a918a752412f9f08ae95
alisa-c <alisachuvilskaya@gmail.com>
2018-10-28 22:01:02 +0000

    step 4 and 5

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
+//step 2
 var https = require('https');
+// console.log('I did it!');
 
-console.log('I did it!');
+//step 3
+var options = {
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Respone: ', response);
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
